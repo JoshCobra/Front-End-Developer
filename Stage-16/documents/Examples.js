d8ea0f7f6d2d3b44b042a7aa969e8b74b549b809
JoshCobra <hulls.reach-0p@icloud.com>
2024-12-04 10:10:33 -0600

    Add examples of default parameters, multiline strings, and template literals to Stage-16 documentation

--- Stage-16/documents/Examples.js
+++ Stage-16/documents/Examples.js
@@ -12,16 +12,47 @@ const arrow = (arg) => { // Syntax of Arrow Function
     console.log(arg);
 }
 
-arrow("hello");
+arrow("hello"); // hello
 
 const name = (n) => { return n };
-const f  = name('World');
+const f  = name("World");
 
-console.log(f)
+console.log(f); // World
 
 // implicit return in arrow functions 
 
 const nameTwo = (n) => n
 const d  = nameTwo('implicit return');
 
-console.log(d);
+console.log(d); // implicit return
+
+const sum = x => x+x
+const e = sum(12);
+
+console.log(e) // 24
+
+// ----------------------------------------
+
+
+// Default Parameters
+
+const presentation = (name = "generic") => { //"generic" is default parameter for name, accepts any type
+    console.log("My name is " + name);
+}
+
+presentation(); // My name is generic
+presentation("josh"); // My name is josh
+
+// ----------------------------------------
+
+// Multiline Strings
+
+let string = "string" //normal string
+
+string = `string with
+multiple lines are
+allowed using backtick` //multiline string
+
+
+// Template Literals
+
