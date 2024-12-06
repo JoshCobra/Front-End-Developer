c524d19b25391d7c5e89cd44676cb96b03f74db3
JoshCobra <hulls.reach-0p@icloud.com>
2024-12-06 10:13:07 -0600

    Add bark and birthday methods to Dog class; update instance names and examples

--- Stage-16/documents/classes-local-storage.js
+++ Stage-16/documents/classes-local-storage.js
@@ -10,10 +10,22 @@ class Dog { //Definition of the class
     giveInfo() {
         return `Dog named ${this.name}, age ${this.age}, color ${this.color}, weight ${this.weight}, size ${this.size}`
     }
+
+    bark() {
+        return `${this.name}: Woof Woof!!!`
+    }
+
+    birthday() {
+        this.age ++
+        return `Dogs BirthDay!!!!, New age is: ${this.age}`
+    }
 };
 
-const dog1 = new Dog("robin",4, "white", 2, "petite")
+const dog1 = new Dog("Robin",4, "white", 2, "petite")
 const dog2 = new Dog("Blacky",12, "black", 25, "medium-big")
 
-dog1.giveInfo()
-dog2.giveInfo()
+dog1.giveInfo() // 'Dog named robin, age 4, color white, weight 2, size petite'
+dog2.giveInfo() // 'Dog named Blacky, age 12, color black, weight 25, size medium-big'
+
+dog2.birthday() // 'Dogs BirthDay, New age is: 13'
+dog1.bark() // 'Robin Woof Woof!!!'
