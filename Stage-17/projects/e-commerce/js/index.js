3f6a4be15f02d699eaaac37544cf31ed14bb4a18
JoshCobra <hulls.reach-0p@icloud.com>
2024-12-11 15:08:56 -0600

    Implement cart and menu toggle functionality with enhanced styles and transitions

---
+++ Stage-17/projects/e-commerce/js/index.js
@@ -0,0 +1,51 @@
+console.log("Josh's eCommerce (Tienda Cobra)")
+// Burger Menu Toggle
+const menuIcon = document.querySelector(".header__icon-menu")
+
+menuIcon.addEventListener("click", () => {
+    document.querySelector(".menu").classList.toggle('open');
+})
+
+document.querySelector(".menu__closeIcon").addEventListener("click", () => {
+    document.querySelector(".menu").classList.remove('open');
+})
+
+
+// Cart Toggle 
+const cartIcon = document.querySelector(".header__icon-cart")
+// console.log(cartIcon)
+
+cartIcon.addEventListener("click", () => {
+    document.querySelector(".cart").classList.toggle('open')
+})
+
+document.querySelector(".cart__closeIcon").addEventListener("click", () => {
+    document.querySelector(".cart").classList.remove('open');
+})
+
+// Add To Cart
+const products__items = document.getElementsByClassName("products__item")
+const cart = document.querySelector(".cart")
+const button = document.getElementsByClassName("products-item__btn")
+
+Array.from(button).forEach(btn => {
+    btn.addEventListener("click", (event) => {
+        const productItem = event.target.closest(".products__item");
+        const productClone = productItem.cloneNode(true);
+        productClone.classList = "cart_item";
+        productClone.classList.add("remove")
+
+        
+        cart.appendChild(productClone);
+    });
+}); 
+
+
+// Remove From Cart
+const removeItem = document.querySelectorAll(".remove")
+
+removeItem.forEach(elem => {
+    elem.addEventListener("click", () => {
+        elem.remove();
+    })
+})
