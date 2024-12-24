03fdc79338b20658dfbde92ca0f41e3c6e96d8b4
JoshCobra <hulls.reach-0p@icloud.com>
2024-12-23 19:40:46 -0600

    Refactor Basketball ScoreBoard project: improve button styling, update score functions, and replace header with button for score reset

--- Stage-17/projects/Basketball-ScoreBoard/index.js
+++ Stage-17/projects/Basketball-ScoreBoard/index.js
@@ -1,53 +1,33 @@
+// Elementos del DOM
 const team1Score = document.getElementById("team1-score")
 const team2Score = document.getElementById("team2-score")
 
+// Variables de puntuación
 let team1Counter = 0
 let team2Counter = 0
 
+// Inicialización de los puntajes y el temporizador
 team1Score.textContent = team1Counter;
 team2Score.textContent = team2Counter;
 
 
+// Funciones para actualizar los puntajes
+const team1Add1Points = () => { team1Counter +=1; updateScore(); }
+const team1Add2Points = () => { team1Counter +=2; updateScore(); }
+const team1Add3Points = () => { team1Counter +=3; updateScore(); }
 
-const team1Add1Points = () => {
-    team1Counter +=1;
+
+const team2Add1Points = () => { team2Counter +=1; updateScore(); }
+const team2Add2Points = () => { team2Counter +=2; updateScore(); }
+const team2Add3Points = () => { team2Counter +=3; updateScore(); }
+
+
+
+function updateScore() {
     team1Score.textContent = team1Counter;
-    verifyScore();
-}
-
-const team1Add2Points = () => {
-    team1Counter +=2;
-    team1Score.textContent = team1Counter;
-    verifyScore();
-}
-
-const team1Add3Points = () => {
-    team1Counter +=3;
-    team1Score.textContent = team1Counter;
-    verifyScore();
-}
-
-
-
-const team2Add1Points = () => {
-    team2Counter +=1;
     team2Score.textContent = team2Counter;
     verifyScore();
 }
-
-const team2Add2Points = () => {
-    team2Counter +=2;
-    team2Score.textContent = team2Counter;
-    verifyScore();
-}
-
-const team2Add3Points = () => {
-    team2Counter +=3;
-    team2Score.textContent = team2Counter;
-    verifyScore();
-}
-
-
 
 function highlight(team) {
     team.style.borderTop = "solid";
@@ -69,13 +49,12 @@ function verifyScore() {
         highlight(team2Score);
 
     } else {
-        console.log("Equipos Empatados")
+        unHighlight(team1Score);
+        unHighlight(team2Score);
     }
 }
 
-
-
-const resetScore = () => {
+function resetScore() {
     team1Counter = 0;
     team2Counter = 0;
 
