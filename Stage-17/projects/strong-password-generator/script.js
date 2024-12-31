78251bb3cb7bc9e3139abc741059148f8c591c13
JoshCobra <hulls.reach-0p@icloud.com>
2024-12-30 21:07:03 -0600

    Add user interaction section with special character toggle for password generation

--- Stage-17/projects/strong-password-generator/script.js
+++ Stage-17/projects/strong-password-generator/script.js
@@ -4,11 +4,15 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
      "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{",
      "[","}","]",",","|",":",";","<",">",".","?","/"];
 
-// const noSpecialCharacters;
+const noSpecialCharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q",
+    "R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l",
+    "m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5",
+     "6", "7", "8", "9"];
 
 const passwordLength = 15;
 
-const passwordsSection = document.getElementById("passwords-section")
+const passwordsSection = document.getElementById("passwords-section");
+const checkBox = document.querySelector("input[name=checkbox]");
 
 const firstPasswordEl = document.getElementById("firstPassword-el");
 const secondPasswordEl = document.getElementById("secondPassword-el");
