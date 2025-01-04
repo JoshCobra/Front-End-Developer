f9d99f11ef5232cd246ebed503f6a96a771f6662
JoshCobra <hulls.reach-0p@icloud.com>
2025-01-04 17:39:18 -0600

    Remove Firebase integration and manifest for Strong Password Generator

--- Stage-17/projects/strong-password-generator/script.js
+++ Stage-17/projects/strong-password-generator/script.js
@@ -1,13 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-database.js";
-
-const firebaseConfig = {
-    databaseURL: process.env.DATABASE_URL
-}
-
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q",
     "R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l",
     "m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5",
