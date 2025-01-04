a5338f6dbdd1954fb850a4ad699aca2ed016f67a
JoshCobra <hulls.reach-0p@icloud.com>
2025-01-04 16:28:31 -0600

    Add Firebase integration and manifest for Strong Password Generator

--- Stage-17/projects/strong-password-generator/script.js
+++ Stage-17/projects/strong-password-generator/script.js
@@ -1,3 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+
+const firebaseConfig = {
+
+}
+
+const app = initializeApp(firebaseConfig);
+
+
+
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q",
     "R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l",
     "m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5",
