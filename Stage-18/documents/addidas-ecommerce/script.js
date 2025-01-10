3e5b41486952cde3ab6051b1587d8fd448dfebc7
JoshCobra <hulls.reach-0p@icloud.com>
2025-01-10 13:54:23 -0600

    Add discount label to product items and enhance styles for Adidas eCommerce project

--- Stage-18/documents/addidas-ecommerce/script.js
+++ Stage-18/documents/addidas-ecommerce/script.js
@@ -1,1 +1,8 @@
 // jQuery Script
+// $(selector).action();
+
+$(document).ready( function() { // All jQuery code, similar to 'defer'
+    
+    $("#item-1 .products__title").before("<span class='discount'> SALE 25% DISCOUNT</span>")
+    $("#item-2 .products__title").before("<span class='discount'> SALE 25% DISCOUNT</span>")
+})
