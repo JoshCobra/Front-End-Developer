9b6769cda633aad7f96204b6d8b485b44f41f167
JoshCobra <hulls.reach-0p@icloud.com>
2025-01-10 14:35:10 -0600

    Update discount label text and enhance styles for better presentation in Adidas eCommerce project

--- Stage-18/documents/addidas-ecommerce/script.js
+++ Stage-18/documents/addidas-ecommerce/script.js
@@ -3,6 +3,6 @@
 
 $(document).ready( function() { // All jQuery code, similar to 'defer'
     
-    $("#item-1 .products__title").before("<span class='discount'> SALE 25% DISCOUNT</span>")
-    $("#item-6 .products__title").before("<span class='discount'> SALE 25% DISCOUNT</span>")
+    $("#item-1 .products__title").before("<span class='discount'> SALE 25% OFF</span>")
+    $("#item-6 .products__title").before("<span class='discount'> SALE 25% OFF</span>")
 })
