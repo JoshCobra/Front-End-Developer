21859eb5df3d554fe8cdc2a0621415385a5a3996
JoshCobra <hulls.reach-0p@icloud.com>
2025-01-10 17:25:09 -0600

    Add navigation menu and enhance discount label presentation in Adidas eCommerce project; improve icon functionality with click events

--- Stage-18/documents/addidas-ecommerce/script.js
+++ Stage-18/documents/addidas-ecommerce/script.js
@@ -3,6 +3,18 @@
 
 $(document).ready( function() { // All jQuery code, similar to 'defer'
     
-    $("#item-1 .products__title").before("<span class='discount'> SALE 25% OFF</span>")
-    $("#item-6 .products__title").before("<span class='discount'> SALE 25% OFF</span>")
+    $("#item-1 .products__title").before("<span class='discount'> 🔥SALE!</span>")
+    $("#item-6 .products__title").before("<span class='discount'> 🔥SALE!</span>")
+
+    $("#icon__shopBag").click( function() {
+        console.log("sopping bag clicked")
+    })
+
+    $("#icon__user").click( function() {
+        console.log("user clicked")
+    })
+
+    $("#icon__menu").click( function() {
+        $(".header__nav--menu").toggleClass("show__nav")
+    })
 })
