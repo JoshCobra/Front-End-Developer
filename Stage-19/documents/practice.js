aa796e097b5a05638297d46346e9722a242c2e47
JoshCobra <hulls.reach-0p@icloud.com>
2025-01-22 18:55:10 -0600

    Add examples of Promises and async/await in practice.js for racing and salad preparation scenarios

---
+++ Stage-19/documents/practice.js
@@ -0,0 +1,130 @@
+// Usando Promises
+// Este ejercicio es la preparacion de un carro para una carrera
+function prepareTires() {
+  
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        
+        let tiresReady = true
+        
+        if(tiresReady) {
+            resolve("Las llantas estan listas 🧑‍🔧")
+        } else {
+          reject("Llantas INCORRECTAS 😲")
+        }
+    }, 2500)
+    });
+  }
+  
+  function fillTheTank() {
+    
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        
+        let tankFilled = true 
+        
+        if(tankFilled) {
+          resolve("Combustible lleno y Listo 🛢️")
+        } else {
+          reject("NO esta lleno el tanque ⚠️")
+        }
+    }, 2500)
+    });
+  }
+  
+  function goToRacingLine() {
+    
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        
+      let inRacingLine = false
+      
+      if(inRacingLine) {
+        resolve("LISTO PARA LA CARRERA 🏎️")
+      } else {
+        reject("Aun no en la linea de meta ⛔")
+      }
+    }, 1000)
+    });
+  }
+  
+  prepareTires().then(value => { console.log(value); return fillTheTank(); })
+           .then(value => { console.log(value); return goToRacingLine(); })
+           .then(value => { console.log(value); console.log("🏁A CORRER🏁"); })
+           .catch(error => console.error(error))
+  
+  
+  
+  // Usando Await y Async
+  // Este codigo hace referencia a una lista de compras para hacer una ensalada
+  
+  function buyVegetables() {
+    
+    let vegetablesList = ["lechuga", "zanahoria", "tomate", "cebolla"]
+    
+    return new Promise((resolve, reject) => {
+      if (vegetablesList.includes("lechuga") && vegetablesList.includes("tomate") ) {
+        resolve("Los Vegetales estan listos")
+      } else {
+        reject("Aun falta un vegetal importante")
+      }
+    });
+  }
+  
+  function buyToppings() {
+    
+    const toppingsBuyed = true
+    
+    return new Promise((resolve, reject) => {
+      if (toppingsBuyed) {
+        resolve("Toppings listos")
+      } else {
+        reject("Aun falta comprar los toppings")
+      }
+    });
+  }
+  
+  async function makeSalad() {
+    
+    try {
+      const vegetablesReady = await buyVegetables()
+      console.log(vegetablesReady)
+  
+      const toppingsReady = await buyToppings()
+      console.log(toppingsReady)
+  
+      console.log("Ensalada Lista ")
+    }
+    catch(error) {
+      console.error(error)
+    }
+  }
+  
+  makeSalad()
+  
+  
+  // Ejercicio 3
+  
+  function randomDice() {
+    const dice = Math.floor( Math.random() * 6)
+    
+    return new Promise((resolve, reject) => {
+      if (dice === 0) {
+        reject("El dado ha caido en 0")
+      } else {
+        resolve(`El dado cayo en ${dice}`)
+      }
+    });
+  } 
+  
+  async function throwDice() {
+    try {
+      const diceReady = await randomDice()
+      console.log(diceReady)
+    } 
+    catch(error) {
+      console.log(error)
+    }
+  }
+  
+  throwDice()
