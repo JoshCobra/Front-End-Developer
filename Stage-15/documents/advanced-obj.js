89324fc1a75cd1626b588767107a023d51918388
JoshCobra <hulls.reach-0p@icloud.com>
2025-02-25 10:49:16 -0600

    Add advanced object manipulation examples in advanced-obj.js

---
+++ Stage-15/documents/advanced-obj.js
@@ -0,0 +1,38 @@
+// JavaScript Objects
+let person = {
+    name: "josh",
+    age: 22,
+    greet() {
+        console.log(`greetings my name is ${this.name} and i am ${this.age} years old`)
+    }
+}
+
+person.greet()
+
+console.log(person.__proto__)
+console.log(person.age)
+console.log(Object.getPrototypeOf(person))
+
+person.sayAge = function() {
+    console.log(`My Age is ${this.age}`)
+}
+
+person.sayAge()
+
+console.log(person)
+
+// Inherit
+let programmer = Object.create(person)
+
+programmer.name = "Krissia"
+programmer.ocupation = "Programmer"
+programmer.avalible = false
+
+console.log(programmer)
+programmer.greet() // Inheriting the functions from original object person
+
+
+// Working with object
+const checkAvalible = (object) => {
+    
+}
