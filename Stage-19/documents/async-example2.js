879a79375f0d4d43db748ff957696e10939c5a09
JoshCobra <hulls.reach-0p@icloud.com>
2025-02-26 18:32:00 -0600

    Add comments to async-example2.js explaining event loop and blocking behavior

--- Stage-19/documents/async-example2.js
+++ Stage-19/documents/async-example2.js
@@ -5,4 +5,10 @@ console.log("Start")
 
 for (let i = 0; i < 999; i++) { }
 
+// In this code we simulate the call to an backend data base so if we add '99999999' the process will be blocked until the bucle is finished
 console.log("End")
+
+ // Event Loop
+ // - Call Stack 
+ // - Web APIs or Node.js
+ // Task Queue and MicrotaskQueue 
