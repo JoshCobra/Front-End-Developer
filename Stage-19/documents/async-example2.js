ae699f173e63e1b57b833730c3c276c6cfcf1323
JoshCobra <hulls.reach-0p@icloud.com>
2025-02-26 18:39:52 -0600

    Enhance comments in async-example2.js to provide a detailed explanation of the event loop and its components

--- Stage-19/documents/async-example2.js
+++ Stage-19/documents/async-example2.js
@@ -8,7 +8,16 @@ for (let i = 0; i < 999; i++) { }
 // In this code we simulate the call to an backend data base so if we add '99999999' the process will be blocked until the bucle is finished
 console.log("End")
 
- // Event Loop
- // - Call Stack 
- // - Web APIs or Node.js
- // Task Queue and MicrotaskQueue 
+ // EVENT LOOP
+ // Components of Event Loop
+
+ // 1.- Call Stack 
+ // 2.- Web APIs or Node.js: setTimeout()...
+ // 3.- Task Queue and Microtask Queue 
+
+ // Event Loop:
+ // 1. Call Stack
+ // 2. If Asynchronous Tasks -> Web APIs or Node.js
+ // 3. When task finished -> Task Queue or Microtask Queue
+ // 4. Checks is Call Stack is empty -> Moves Microtask Queue tasks or Task Queue tasks to Call Stack
+ // 5. Process Repeats
