cb67c33b717ea4d4c74672cd88ee985972ede12d
JoshCobra <hulls.reach-0p@icloud.com>
2025-03-03 10:28:22 -0600

    Add asynchronous code examples and explanations to async-example2.js

--- Stage-19/documents/async-example2.js
+++ Stage-19/documents/async-example2.js
@@ -21,3 +21,24 @@ console.log("End")
  // 3. When task finished -> Task Queue or Microtask Queue
  // 4. Checks is Call Stack is empty -> Moves Microtask Queue tasks or Task Queue tasks to Call Stack
  // 5. Process Repeats
+
+ // Async Code
+console.log("--------")
+ // - Callbacks - Its a function passed to another function to execute when the principal function its finished
+
+ console.log("Start")
+
+ setTimeout(()=> { // Executes before the timeout
+    console.log("Time Out")
+ }, 2000) // Defined timeout of 2 seconds 
+
+ console.log("Finish")
+
+
+ // Example 
+
+ function step1(callback) {
+    setTimeout(() => {
+        console.log("Step - 1 Done")
+    })
+ }
