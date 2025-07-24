9330908af1333b6faaa36fcf4590bc59884dfa4a
Josh <hulls.reach-0p@icloud.com>
2025-07-24 16:43:04 -0600

    Add initial TypeScript files for main functionality

---
+++ Stage-22/practice/js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var name = "Josue";
+var valor2 = 2;
+var valor3 = false;
+//To compile, go to the terminal, and in the folder of the archive select the archive and execute the next command:
+// tsc {archive.ts}
+// And just like that automatically, typescript exports your archive and transforms to an dot js file
+// We can also use the command tsc {archive-name.ts} -watch to compile in real time
---
+++ Stage-22/practice/js/main.ts
@@ -0,0 +1,14 @@
+export{}
+
+const name: string = "Josue"
+const valor2 : number = 2
+const valor3 : boolean = false
+
+export{}
+
+//To compile, go to the terminal, and in the folder of the archive select the archive and execute the next command:
+// tsc {archive.ts}
+
+// And just like that automatically, typescript exports your archive and transforms to an dot js file
+
+// We can also use the command tsc {archive-name.ts} -watch to compile in real time
